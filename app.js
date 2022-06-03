1ecb1f5c340d94c775c8efb40c926f478370c309
ktkthuong <ktkthuong@gmail.com>
2022-06-04 00:41:04 +0700

    Doc body cua request

--- app.js
+++ app.js
@@ -12,7 +12,17 @@ const server = http.createServer((req, res) => {
         return res.end();
     }
     if (url ==='/message' && method ==='POST'){
-        fs.writeFileSync('message.text', 'DUMMY');
+        const body = [];
+        req.on('data', (chunk) =>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () =>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message=parsedBody.split('=')[1];
+            fs.writeFileSync('message.text', message);
+        });
+        
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
