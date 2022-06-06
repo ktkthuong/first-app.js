a77b85c80c8f37910eeaf6fc93fc2659f5674c67
ktkthuong <ktkthuong@gmail.com>
2022-06-06 23:44:10 +0700

    Cai dat middleware

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('In the middleware!');//Allow the request to continue to the next middleware in line
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+});
+
 const server = http.createServer(app);
 
 server.listen(3002);
