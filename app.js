ac24b582a187dda9b01d1feb95a1d6e976c39169
ktkthuong <ktkthuong@gmail.com>
2022-06-07 00:23:59 +0700

    Xử lý các Route khác nhau

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-const http = require('http');
+//const http = require('http'); cách 1 hiển thị chữ Hello from express! ra trang html.
 
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');//Allow the request to continue to the next middleware in line
+// app.use((req, res, next) => {
+//     console.log('In the middleware!');//Allow the request to continue to the next middleware in line
+//     next();
+// });
+app.use('/',(req, res, next) => {
+    console.log('This always run!');
     next();
 });
 
-app.use((req, res, next) => {
+
+app.use('/add-product',(req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>The "Add product" Page</h1>');
+});
+
+
+app.use('/',(req, res, next) => {
     console.log('In another middleware!');
     res.send('<h1>Hello from express!</h1>');
 });
 
-const server = http.createServer(app);
+// const server = http.createServer(app); cách 1
 
-server.listen(3002);
+// server.listen(3002); cách 1
+app.listen(3002);//cách 2 y chang, nhanh hơn
