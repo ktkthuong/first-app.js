7975f7fb9240836af4ba4fff5985e3b466942396
ktkthuong <ktkthuong@gmail.com>
2022-06-07 21:58:49 +0700

    Phân tích cú pháp Requests đến

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
 //const http = require('http'); cách 1 hiển thị chữ Hello from express! ra trang html.
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+
 
 // app.use((req, res, next) => {
 //     console.log('In the middleware!');//Allow the request to continue to the next middleware in line
 //     next();
 // });
-app.use('/',(req, res, next) => {
-    console.log('This always run!');
-    next();
-});
+app.use(bodyParser.urlencoded({extended: false})); 
 
 
 app.use('/add-product',(req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>The "Add product" Page</h1>');
+    
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 
 app.use('/',(req, res, next) => {
-    console.log('In another middleware!');
+    
     res.send('<h1>Hello from express!</h1>');
 });
 
