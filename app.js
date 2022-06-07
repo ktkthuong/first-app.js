546343d6b8393fa963cf35baf77e6c82b72b4187
ktkthuong <ktkthuong@gmail.com>
2022-06-07 23:24:43 +0700

    Sử dụng Express Router

--- app.js
+++ app.js
@@ -1,36 +1,14 @@
-//const http = require('http'); cách 1 hiển thị chữ Hello from express! ra trang html.
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-
-// app.use((req, res, next) => {
-//     console.log('In the middleware!');//Allow the request to continue to the next middleware in line
-//     next();
-// });
 app.use(bodyParser.urlencoded({extended: false})); 
 
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/add-product',(req, res, next) => {
-    
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-
-app.use('/',(req, res, next) => {
-    
-    res.send('<h1>Hello from express!</h1>');
-});
-
-// const server = http.createServer(app); cách 1
-
-// server.listen(3002); cách 1
-app.listen(3002);//cách 2 y chang, nhanh hơn
+app.listen(3002);
