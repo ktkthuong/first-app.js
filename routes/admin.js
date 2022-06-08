0e77985544652215666e1db7c075fce9c43b8fd1
ktkthuong <ktkthuong@gmail.com>
2022-06-08 23:30:59 +0700

    Sử dụng chức năng trợ giúp để điều hướng

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,12 @@
 const path = require('path');
 const express = require('express');
 const router = express.Router();
+const rootDir = require('../util/path');
 
 // /admin/add-product => GET
 router.get('/add-product',(req, res, next) => {
     
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
---
+++ util/path.js
@@ -0,0 +1,6 @@
+const path = require('path');
+
+
+module.exports = path.dirname(process.mainModule.filename);
+
+
