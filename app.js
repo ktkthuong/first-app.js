fb6cfd27ba3fedc91f1ce5e2e235be6d6f0956f9
ktkthuong <ktkthuong@gmail.com>
2022-06-09 22:11:18 +0700

    Chia sẻ dữ liệu giữa các Requests, User

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false})); 
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-app.use('/admin',adminRoutes);
+app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 const rootDir = require('../util/path');
+const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product',(req, res, next) => {
@@ -12,8 +13,9 @@ router.get('/add-product',(req, res, next) => {
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    products.push({title: req.body.title});
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
