ab533ee125c4e73907671d5f29bd124bdefa05e3
ktkthuong <ktkthuong@gmail.com>
2022-06-11 15:27:44 +0700

    Thêm bố cục vào Handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,15 @@ const router = express.Router();
 
 router.get('/',(req, res, next) => {
     const products = adminData.products;
-    res.render('shop',{prods:products, pageTitle:'Shop', path:'/', hasProducts: products.length > 0});
+    res.render('shop',{
+        prods:products, 
+        pageTitle:'Shop', 
+        path:'/', 
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCss: true,
+        
+    });
 });
 
 module.exports = router;
