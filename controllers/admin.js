c6977d827ce580ce806fb0bcaa412cfa85d6c244
ktkthuong <ktkthuong@gmail.com>
2022-06-17 22:48:11 +0700

    sửa nút add product thành update product nhưng bị lỗi

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,7 +4,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    
+    editing: false
   });
 };
 
@@ -23,11 +23,19 @@ exports.getEditProduct = (req, res, next) => {
   if(!editMode) {
     return res.redirect('/');
   }
-  res.render('admin/edit-product', {
-    pageTitle: 'Edit Product',
-    path: '/admin/edit-product',
-    editing: editMode
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if(!product){
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    });
   });
+  
 };
 
 exports.getProducts = (req, res, next) => {
