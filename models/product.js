35703dbf6d96276fc2c763b23780bdaff203308b
ktkthuong <ktkthuong@gmail.com>
2022-06-18 22:38:30 +0700

    Thêm chức năng xóa sản phẩm

--- models/product.js
+++ models/product.js
@@ -45,6 +45,17 @@ module.exports = class Product {
     });
   }
 
+  static deleteById(id){
+    getProductsFromFile(products => {
+      const updatedProducts = products.filter(prod => prod.id === id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if(!err){
+          
+        }
+      });
+    });
+  }
+
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
