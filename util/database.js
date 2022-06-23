b0a4202e5e83ebea7cf6c83f84e72585bef1e5fb
ktkthuong <ktkthuong@gmail.com>
2022-06-23 20:54:01 +0700

    Kết nối với Cơ sở dữ liệu

--- util/database.js
+++ util/database.js
@@ -1,11 +1,7 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: '30062010phat'
-
+const sequelize = new Sequelize('node-complete','root', '30062010phat', {
+    dialect: 'mysql', 
+    host: 'localhost'
 });
-
-module.exports = pool.promise();
+module.exports = sequelize;
