91f8685018747b1f5c0b017840d990c25a9eabb5
ktkthuong <ktkthuong@gmail.com>
2022-06-24 18:58:31 +0700

    Truy xuất dữ liệu & tìm kiếm sản phẩm

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,16 +2,17 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows, fieldData]) => {
+  Product.findAll()
+  .then(products => {
     res.render('shop/product-list', {
-      prods: rows,
+      prods: products,
       pageTitle: 'All Products',
       path: '/products'
     });
   })
-  .catch(err => console.log(err));
-  
+  .catch(err =>{
+    console.log(err);
+  });     
 };
 
 exports.getProduct = (req, res, next) => {
@@ -24,21 +25,21 @@ exports.getProduct = (req, res, next) => {
       path : '/products'
     });
   })
-  .catch(err => console.log(err));
- 
+  .catch(err => console.log(err)); 
 }
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows, fieldData]) => {
+  Product.findAll()
+  .then(products => {
     res.render('shop/index', {
-      prods: rows,
+      prods: products,
       pageTitle: 'Shop',
       path: '/'
     });
   })
-  .catch(err => console.log(err));
-  
+  .catch(err =>{
+    console.log(err);
+  });  
 };
 
 exports.getCart = (req, res, next) => {
