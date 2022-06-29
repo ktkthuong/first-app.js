e9c0b36ea7e40bb1b1c1a3a25eee58ca37b771e9
ktkthuong <ktkthuong@gmail.com>
2022-06-29 13:38:06 +0700

    Thêm các sản phẩm hiện có & lấy các mặt hàng trong giỏ hàng

--- controllers/shop.js
+++ controllers/shop.js
@@ -83,16 +83,18 @@ exports.postCart = (req, res, next) => {
     if(products.length > 0){
       product = products[0];
     }
-    let newQuantity = 1;
+    // let newQuantity = 1;
     if (product){
-      //...
+      const oldQuantity = product.cartItem.quantity;
+      newQuantity = oldQuantity + 1 ;
+      return product;      
     }    
     return Product.findByPk(prodId)
-    .then(product => {
-      return fetchedCart.addProduct(product, {
-        through: {quantity: newQuantity}});
-    })
-    .catch(err => console.log(err));
+  })
+  .then(product => {
+    return fetchedCart.addProduct(product, {
+      through: {quantity: newQuantity}
+    });
   })
   .then(()=>{ 
     res.redirect('/cart');
