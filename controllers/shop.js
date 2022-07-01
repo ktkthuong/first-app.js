ae91d0f18555bc5620ec2df6c13deb086ee46faf
ktkthuong <ktkthuong@gmail.com>
2022-07-01 21:58:21 +0700

    Đặt lại giỏ hàng, tìm nạp và xuất đơn hàng

--- controllers/shop.js
+++ controllers/shop.js
@@ -121,9 +121,11 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
   .getCart()
   .then(cart => {
+    fetchedCart = cart; 
     return cart.getProducts();
   })
   .then(products => {
@@ -138,7 +140,10 @@ exports.postOrder = (req, res, next) => {
       );
    })
    .then(result => {
-      res.redirect('/orders');
+      return fetchedCart.setProducts(null);      
+   })
+   .then(result =>{
+    res.redirect('/orders');
    })
    .catch(err => console.log(err));   
   })
@@ -146,15 +151,21 @@ exports.postOrder = (req, res, next) => {
 }
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
+  req.user
+  .getOrders({include: ['products']})
+  .then(orders => {
+    res.render('shop/orders', {
+      path: '/orders',
+      pageTitle: 'Your Orders',
+      orders: orders
+    });
+  })
+  .catch(err => console.log(err));  
 };
 
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
-  });
-};
+// exports.getCheckout = (req, res, next) => {
+//   res.render('shop/checkout', {
+//     path: '/checkout',
+//     pageTitle: 'Checkout'
+//   });
+// };
