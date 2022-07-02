eb5f3b19d404c8970669b70fca4d54f064030eb5
ktkthuong <ktkthuong@gmail.com>
2022-07-02 21:23:49 +0700

    Cài đặt trình điều khiển MongoDB

--- util/database.js
+++ util/database.js
@@ -1,7 +1,15 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete','root', '30062010phat', {
-    dialect: 'mysql', 
-    host: 'localhost'
-});
-module.exports = sequelize;
+const mongoConnect = (callback) =>{
+    MongoClient.connect('mongodb+srv://ktkthuong:30062010phat@cluster0.gwa1tdk.mongodb.net/?retryWrites=true&w=majority')
+        .then(client => {
+            console.log('Connected!');
+            callback(client);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+module.exports = mongoConnect;
+
