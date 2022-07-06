c13fec36baf60c1a353da50a8fabd8551f7708cb
ktkthuong <ktkthuong@gmail.com>
2022-07-06 23:09:08 +0700

    Làm việc trên các mặt hàng & đơn đặt hàng trong giỏ hàng

--- models/user.js
+++ models/user.js
@@ -4,14 +4,31 @@ const getDb = require('../util/database').getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-    constructor(username, email){
+    constructor(username, email, cart, id){
         this.name = username;
         this.email = email;
+        this.cart = cart; // {item: []}
+        this._id = id;
     }
     save(){
         const db = getDb();
         return db.collection('users').insertOne(this);
     }
+
+    addToCart(product) {
+        // const cartProduct = this.cart.item.findIndex(cp => {
+        //     return cp._id === product._id;
+        // });        
+        const updatedCart = {items: [{...product, quantity: 1 }]};
+        const db = getDb();
+        return db
+        .collection('users')
+        .updateOne(
+            {_id: new ObjectId(this._id)},
+            {$set: {cart: updatedCart}}
+        );
+    }
+
     static findByPk(userId){
         const db = getDb();
         return db
