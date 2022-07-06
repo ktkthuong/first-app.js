b8d56182c472e3fa60c26ab94af7697772853652
ktkthuong <ktkthuong@gmail.com>
2022-07-06 20:31:15 +0700

    Tạo người dùng mới

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,12 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) =>{
-    // User.findByPk(1)
-    // .then(user => {
-    //     req.user = user;
-    //     next();
-    // })
-    // .catch(err => console.log(err));
+    User.findByPk('62c58cca728ffa8140ef96dc')
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => console.log(err));
     next();
 });
 
@@ -32,7 +33,7 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {    
+mongoConnect(() => {
     app.listen(3002);
 });
 
