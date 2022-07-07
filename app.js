378e66bdb7e70547ca3735ffb25894e8138c44cf
ktkthuong <ktkthuong@gmail.com>
2022-07-07 16:22:54 +0700

    Thêm chức năng Thêm vào giỏ hàng

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) =>{
     User.findByPk('62c58cca728ffa8140ef96dc')
     .then(user => {
-        req.user = user;
+        req.user = new User(user.name, user.email, user.cart, user._id);
         next();
     })
     .catch(err => console.log(err));
--- models/user.js
+++ models/user.js
@@ -19,7 +19,7 @@ class User {
         // const cartProduct = this.cart.item.findIndex(cp => {
         //     return cp._id === product._id;
         // });        
-        const updatedCart = {items: [{...product, quantity: 1 }]};
+        const updatedCart = {items: [{productId: new ObjectId(product._id), quantity: 1 }]};
         const db = getDb();
         return db
         .collection('users')
