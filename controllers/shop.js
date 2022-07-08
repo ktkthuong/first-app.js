47ccc6da551f57fcbbc280480ebc31703fa11891
ktkthuong <ktkthuong@gmail.com>
2022-07-08 23:05:02 +0700

    lab06-21 lỗi xóa các mục trong giỏ hàng

--- controllers/shop.js
+++ controllers/shop.js
@@ -62,7 +62,6 @@ exports.getCart = (req, res, next) => {
         products: products
       });
     })   
- 
   .catch(err => console.log(err));
 };
 
@@ -76,50 +75,13 @@ exports.postCart = (req, res, next) => {
     console.log(result);
     res.redirect('/cart');
   });
-//   let fetchedCart;
-//   let newQuantity = 1;
-//  req.user
-//   .getCart()
-//   .then(cart => {
-//     fetchedCart = cart;
-//     return cart.getProducts({where: {id: prodId}});
-//   })
-//   .then(products =>{
-//     let product;
-//     if(products.length > 0){
-//       product = products[0];
-//     }
-//     // let newQuantity = 1;
-//     if (product){
-//       const oldQuantity = product.cartItem.quantity;
-//       newQuantity = oldQuantity + 1 ;
-//       return product;      
-//     }    
-//     return Product.findByPk(prodId)
-//   })
-//   .then(product => {    
-//     return fetchedCart.addProduct(product, {
-//       through: {quantity: newQuantity}
-//     });
-//   })
-//   .then(()=>{ 
-//     res.redirect('/cart');
-//   })
-//   .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-  .getCart()
-  .then(cart => {
-    return cart.getProducts({where: {id: prodId}});
-  })
-  .then(products =>{
-    const product = products[0];
-    return product.cartItem.destroy();
-  })
-  .then(result =>{
+  .deleteItemFromCart(prodId) 
+  .then(result => {
     res.redirect('/cart');
   })
   .catch(err => console.log(err));  
--- models/user.js
+++ models/user.js
@@ -61,7 +61,20 @@ class User {
                     }).quantity
                  };
             });
+        });        
+    }
+
+    deleteItemFromCart(productId) {
+        const updatedCartItems = this.cart.items.filter(item => {
+            return item.productId.toString() !== productId.toString();
         });
+        const db = getDb();
+        return db
+        .collection('users')
+        .updateOne(
+            {_id: new ObjectId(this._id)},
+            {$set: {cart: {items: updatedCartItems}}}
+        );
     }
 
     static findByPk(userId){
