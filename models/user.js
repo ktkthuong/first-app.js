de029105bd91d7f6847b199b0b08381e278e2738
ktkthuong <ktkthuong@gmail.com>
2022-07-09 20:31:17 +0700

    Nhận đơn đặt hàng

--- models/user.js
+++ models/user.js
@@ -103,7 +103,10 @@ class User {
 
     getOrders() {
         const db = getDb();
-        //return db.collection('orders').
+        return db
+        .collection('orders')
+        .find({'user._id': new ObjectId(this._id)})
+        .toArray();
     }
 
     static findByPk(userId){
