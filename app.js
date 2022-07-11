9774001e6dce907abc8619f86acb560e4fc61925
ktkthuong <ktkthuong@gmail.com>
2022-07-11 23:22:06 +0700

    Thêm và sử dụng model người dùng

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) =>{
-//     User.findByPk('62c58cca728ffa8140ef96dc')
-//     .then(user => {
-//         req.user = new User(user.name, user.email, user.cart, user._id);
-//         next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) =>{
+    User.findById('62cc4d82e1ca9a9ab665f6a0')
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -37,6 +37,19 @@ mongoose
         'mongodb+srv://ktkthuong:30062010phat@cluster0.gwa1tdk.mongodb.net/shop?retryWrites=true&w=majority'
     )
     .then(result => {
+        User.findOne().then(user => {
+            if(!user) {
+                const user = new User({
+                    name: 'Max',
+                    email: 'max@test.com',
+                    cart: {
+                        items: []
+                    }
+                });
+                user.save();
+            }
+        });
+        
         app.listen(3002);
     })
     .catch(err => {
