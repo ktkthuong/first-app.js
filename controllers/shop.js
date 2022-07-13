10cf28318cf3bea8bb12b1870d225a8237f78860
ktkthuong <ktkthuong@gmail.com>
2022-07-13 19:47:58 +0700

    Làm việc với Giỏ hàng

--- controllers/shop.js
+++ controllers/shop.js
@@ -58,7 +58,7 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByPk(prodId)
+  Product.findById(prodId)
   .then(product => {
     return req.user.addToCart(product);
   })
