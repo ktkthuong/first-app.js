7c338c8ce2180af310dd98c438f4bfb7a323205d
ktkthuong <ktkthuong@gmail.com>
2022-07-13 20:25:36 +0700

    Tải giỏ hàng

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const user = require('../models/user');
 
 
 exports.getProducts = (req, res, next) => {
@@ -45,8 +46,9 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => { 
   req.user
-  .getCart()
-  .then(products => {
+  .populate('cart.items.productId')
+  .then(user => {
+    const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
