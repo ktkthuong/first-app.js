755c14f5762e2fc4a997957a8bf95dcaf768aee7
ktkthuong <ktkthuong@gmail.com>
2022-07-20 13:27:57 +0700

    Lab 8.3: Thao tác Cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,8 +2,8 @@ exports.getLogin = (req, res, next) => {
   const isLoggedIn = req
     .get('Cookie')
     .split(';')[1]
-    .trim()
-    .split('=')[1];
+    // .trim()
+    // .split('=')[1] === 'true';
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
