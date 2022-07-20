a5f6f9eb3b42dcf0f857c9ee479cb527275ef6cf
ktkthuong <ktkthuong@gmail.com>
2022-07-20 14:28:41 +0700

    Lab 8.6: Sử dụng Middleware Session

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,8 +2,9 @@ exports.getLogin = (req, res, next) => {
   // const isLoggedIn = req
   //   .get('Cookie')
   //   .split(';')[1]
-    // .trim()
-    // .split('=')[1] === 'true';
+  // .trim()
+  // .split('=')[1] === 'true';
+  console.log(req.session.isLoggedIn);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
@@ -12,6 +13,6 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly');
+    req.session.isLoggedIn = true;
     res.redirect('/');
   };
