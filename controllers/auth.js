ced372575aa7bc1885b8e8fc79fa36e5e9d561fb
ktkthuong <ktkthuong@gmail.com>
2022-07-21 19:55:10 +0700

    Lab 8.15: Thêm chức năng đăng nhập

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,14 +19,31 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('62cc4d82e1ca9a9ab665f6a0')
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({email: email})
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
+      if(!user) {
+        return res.redirect('/login');
+      }
+      bcrypt
+      .compare(password, user.password)
+      .then(doMatch => {
+        if (doMatch) {
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          return req.session.save(err => {          
+            console.log(err);
+            res.redirect('/');
+          });         
+        }
+        res.redirect('/login');
+      })
+      .catch(err => {
         console.log(err);
-        res.redirect('/');
+        res.redirect('/login');
       });
+      
     })
     .catch(err => console.log(err));
 };
