3c2b26c4a703c0ae87a5b8e678cbae338554d3d8
ktkthuong <ktkthuong@gmail.com>
2022-07-22 23:59:20 +0700

    Lab 9.1: Thiết lập và cơ bản về kiểm tra dữ liệu

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
 
+const { validationResult } = require('express-validator/check');
+
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
   if (message.length > 0) {
@@ -66,6 +68,16 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422)
+    .render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()
+    });;
+  }
   User.findOne({email: email})
   .then(userDoc => {
     if(userDoc) {
@@ -95,5 +107,19 @@ exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
     console.log(err);
     res.redirect('/');
+  });
+};
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage: message  
   });
 };
