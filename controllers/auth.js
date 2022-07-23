c6d364a0e646db52a00dc742837d0e2d7460f299
ktkthuong <ktkthuong@gmail.com>
2022-07-23 15:40:56 +0700

    Lab 9.2: Sử dụng thông báo lỗi kiểm tra dữ liệu

--- controllers/auth.js
+++ controllers/auth.js
@@ -75,7 +75,7 @@ exports.postSignup = (req, res, next) => {
     .render('auth/signup', {
       path: '/signup',
       pageTitle: 'Signup',
-      errorMessage: errors.array()
+      errorMessage: errors.array()[0].msg
     });;
   }
   User.findOne({email: email})
