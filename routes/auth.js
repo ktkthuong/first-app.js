118a43c4405b6eecc7d1e57981cc869543125b24
ktkthuong <ktkthuong@gmail.com>
2022-07-24 19:33:05 +0700

    Lab 9.10: Dọn dẹp dữ liệu

--- routes/auth.js
+++ routes/auth.js
@@ -10,46 +10,68 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post(
+  '/login',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Please enter a valid email address.')
+      .normalizeEmail(),
+    body('password', 'Password has to be valid.')
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+      .trim()
+  ],
+  authController.postLogin
+);
 
 router.post(
-    '/signup', 
-    [
-        check('email')
-            .isEmail()
-            .withMessage('Please enter a valid email.')
-            .custom((value, {req}) => {
-                // if (value === 'test@test.com') {
-                //     throw new Error('This email address if forbidden.')
-                // }
-                // return true;
-                return User.findOne({email: value})
-                .then(userDoc => {
-                    if(userDoc) {
-                        return Promise.reject(
-                            'E-mail exists already, please pick a different one.'
-                        );
-                    }
-                });
-            }), 
-        body(
-            'password',
-            'Please enter a password with only numbers and text and at least 5 characters.'
-            )
-        .isLength({min: 5})
-        .isAlphanumeric(),
-        body('confirmpassword').custom((value, {req}) => {
-            if(value !== req.body.password) {
-                throw new Error('Password have to match!');
-            }
-            return true;
-        })        
-    ],
-    authController.postSignup
+  '/signup',
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, { req }) => {
+        // if (value === 'test@test.com') {
+        //   throw new Error('This email address if forbidden.');
+        // }
+        // return true;
+        return User.findOne({ email: value }).then(userDoc => {
+          if (userDoc) {
+            return Promise.reject(
+              'E-Mail exists already, please pick a different one.'
+            );
+          }
+        });
+      })
+      .normalizeEmail(),
+    body(
+      'password',
+      'Please enter a password with only numbers and text and at least 5 characters.'
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+      .trim(),
+    body('confirmPassword')
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Passwords have to match!');
+        }
+        return true;
+      })
+  ],
+  authController.postSignup
 );
 
 router.post('/logout', authController.postLogout);
 
 router.get('/reset', authController.getReset);
+
+router.post('/reset', authController.postReset);
+
+router.get('/reset/:token', authController.getNewPassword);
+
+router.post('/new-password', authController.postNewPassword);
 
 module.exports = router;
