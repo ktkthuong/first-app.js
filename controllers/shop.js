faf12bfe4fc7a4b312f44ecf10415f53b1dae0f9
ktkthuong <ktkthuong@gmail.com>
2022-07-27 23:49:41 +0700

    Lab 10.13: Sử dụng PDFKit để tạo .pdf

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const PDFDocument = require('pdfkit');
+
 const Product = require('../models/product');
 const Order = require('../models/order');
 
@@ -156,6 +158,16 @@ exports.getInvoice = (req, res, next) => {
     }
     const invoiceName = 'invoice-' + orderId + '.pdf';
     const invoicePath = path.join('data', 'invoices', invoiceName);
+
+    const pdfDoc = new PDFDocument();
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
+    pdfDoc.pipe(fs.createWriteStream(invoicePath));
+    pdfDoc.pipe(res);
+
+    pdfDoc.text('Hello World!');
+
+    pdfDoc.end();
     // fs.readFile(invoicePath, (err, data) => {
     //   if(err) {
     //     return next(err);
@@ -164,10 +176,9 @@ exports.getInvoice = (req, res, next) => {
     //   res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
     //   res.send(data);
     // });
-    const file = fs.createReadStream(invoicePath);
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
-    file.pipe(res);
+    // const file = fs.createReadStream(invoicePath);
+    
+    // file.pipe(res);
   })
   .catch(err => next(err));
   
