60776d6e1c55aeaea4445a49270a9f5ad7dad44d
ktkthuong <ktkthuong@gmail.com>
2022-07-28 19:07:22 +0700

    Lab10.20: Chuẩn bị dữ liệu phân trang trên máy chủ

--- controllers/shop.js
+++ controllers/shop.js
@@ -44,15 +44,27 @@ exports.getProduct = (req, res, next) => {
 
 exports.getIndex = (req, res, next) => {
   const page = req.query.page;
+  let totalItems;
 
   Product.find()
-    .skip((page - 1) * ITEM_PER_PAGE)
-    .limit(ITEM_PER_PAGE)
+    .count()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEM_PER_PAGE)
+        .limit(ITEM_PER_PAGE);
+    })
     .then(products => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
-        path: '/'
+        path: '/',
+        totalProducts: totalItems,
+        hasNextPage: ITEM_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEM_PER_PAGE)
       });
     })
     .catch(err => {
